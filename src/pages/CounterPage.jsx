b265cf1d5c9b88719fc8f89712b83da905db88e8
Parrurox <shaikhparwez963@gmail.com>
2024-02-02 18:29:11 +0530

    reducers added

--- src/pages/CounterPage.jsx
+++ src/pages/CounterPage.jsx
@@ -1,15 +1,93 @@
-import useCounterAndLog from "../hooks/use-counter";
-
+/* eslint-disable react/prop-types */
+import { useReducer } from "react";
 import Button from "../components/Button";
+import Panel from "../components/Panel";
+import { produce } from "immer";
 
+const INCREMENT_COUNT = "increment";
+const DECREMENT_COUNT = "decrement";
+const SET_VALUE_TO_ADD = "setValueToAdd";
+const ADD_VALUE_TO_COUNT = "addValueToCount";
+
+//reducers
+const reducer = (state, action) => {
+  switch (action.type) {
+    case INCREMENT_COUNT:
+      state.count++;
+      return;
+    //this is how we can mutate the state in immer (just example here not repeating in other cases to keep the og way for later reference)
+    case DECREMENT_COUNT:
+      return { ...state, count: state.count - 1 };
+    case SET_VALUE_TO_ADD:
+      return { ...state, valueToAdd: action.payload };
+    case ADD_VALUE_TO_COUNT:
+      return { ...state, count: state.count + state.valueToAdd, valueToAdd: 0 };
+
+    default:
+      //we can also throw an error here
+      return state;
+  }
+};
 const CounterPage = ({ initialCount }) => {
-  const { count, increment } = useCounterAndLog(initialCount);
+  // const [count, setCount] = useState(initialCount);
+  // const [valueToAdd, setValueToAdd] = useState(0);
+
+  //using immer hence the produce function
+  const [state, dispatch] = useReducer(produce(reducer), {
+    count: initialCount,
+    valueToAdd: 0,
+  });
+
+  const increment = () => {
+    // setCount(count + 1);
+    dispatch({
+      type: INCREMENT_COUNT,
+    });
+  };
+  const decrement = () => {
+    // setCount(count - 1);
+    dispatch({
+      type: DECREMENT_COUNT,
+    });
+  };
+  const handleChange = (e) => {
+    // or 0 if NaN comes in event for eg. if user hit delete
+
+    dispatch({
+      type: SET_VALUE_TO_ADD,
+      payload: parseInt(e.target.value) || 0,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // setCount(count + valueToAdd);
+    // setValueToAdd(0);
+    dispatch({
+      type: ADD_VALUE_TO_COUNT,
+    });
+  };
+
   //render
   return (
-    <div>
-      <h1>count is {count}</h1>
-      <Button onClick={increment}>Increment</Button>
-    </div>
+    <Panel className="m-3">
+      <h1 className="text-lg">count is {state.count}</h1>
+      <div className="flex flex-row">
+        <Button onClick={increment}>Increment</Button>
+        <Button onClick={decrement}>Decrement</Button>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <label>Add a lot !</label>
+        <input
+          //added or '' to remove the default 0 always present in input
+          value={state.valueToAdd || ""}
+          onChange={handleChange}
+          type="number"
+          className="p-1 m-3 bg-gray-50 border border-gray-300"
+        />
+        <Button>Add it</Button>
+      </form>
+    </Panel>
   );
 };
 
